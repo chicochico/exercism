f2ca03fe17621ec8d82f7db99e11002b22d12a34
chico <fxchiang@gmail.com>
2017-10-07 18:57:08 -0300

    better solution for raindrops in elixir

--- elixir/raindrops/raindrops.exs
+++ elixir/raindrops/raindrops.exs
@@ -10,11 +10,10 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t
   def convert(number) do
-    factors = prime_factors(number)
     conversion = ""
-      |> raindrop(factors, :pling)
-      |> raindrop(factors, :plang)
-      |> raindrop(factors, :plong)
+      |> raindrop(number, :pling)
+      |> raindrop(number, :plang)
+      |> raindrop(number, :plong)
 
     if conversion == "" do
       Integer.to_string(number)
@@ -23,37 +22,16 @@ defmodule Raindrops do
     end
   end
 
-  def raindrop(str, factors, :pling) do
-    if 3 in factors, do: str <> "Pling", else: str
-  end
-
-  def raindrop(str, factors, :plang) do
-    if 5 in factors, do: str <> "Plang", else: str
-  end
-
-  def raindrop(str, factors, :plong) do
-    if 7 in factors, do: str <> "Plong", else: str
-  end
-
-  def is_prime(2), do: true
-  def is_prime(3), do: true
-  def is_prime(n) do
-    upper_limit = :math.sqrt(n) |> :math.floor |> round
-    Enum.all?(2..upper_limit, &(rem(n, &1) != 0))
-  end
-
-  def prime_factors(1), do: []
-  def prime_factors(primes \\ [], n) when is_list(primes) do
-    if is_prime(n) do
-      primes ++ [n]
-    else
-      ls = smallest_factor(n)  # left side
-      rs = n/ls |> :math.floor |> round  # right side
-      prime_factors(primes, ls) |> prime_factors(rs)
+  def raindrop(str, n, drop) do
+    case drop do
+      :pling ->
+        if divisible_by(n, 3), do: str <> "Pling", else: str
+      :plang ->
+        if divisible_by(n, 5), do: str <> "Plang", else: str
+      :plong ->
+        if divisible_by(n, 7), do: str <> "Plong", else: str
     end
   end
 
-  def smallest_factor(n) do
-    Enum.find(2..n, &(rem(n, &1) == 0))
-  end
+  def divisible_by(a, b), do: rem(a, b) == 0
 end
