0abe34c904d59fae23f86b91840745236c16f6dd
chico <fxchiang@gmail.com>
2017-10-18 12:38:51 -0200

    beer-song elixir solution

---
+++ elixir/beer-song/beer_song.exs
@@ -0,0 +1,47 @@
+defmodule BeerSong do
+  @doc """
+  Get a single verse of the beer song
+  """
+  @spec verse(integer) :: String.t
+  def verse(0) do
+    """
+    No more bottles of beer on the wall, no more bottles of beer.
+    Go to the store and buy some more, 99 bottles of beer on the wall.
+    """
+  end
+
+  def verse(1) do
+    """
+    1 bottle of beer on the wall, 1 bottle of beer.
+    Take it down and pass it around, no more bottles of beer on the wall.
+    """
+  end
+
+  def verse(2) do
+    """
+    2 bottles of beer on the wall, 2 bottles of beer.
+    Take one down and pass it around, 1 bottle of beer on the wall.
+    """
+  end
+
+  def verse(number) do
+    """
+    #{number} bottles of beer on the wall, #{number} bottles of beer.
+    Take one down and pass it around, #{number-1} bottles of beer on the wall.
+    """
+  end
+
+  @doc """
+  Get the entire beer song for a given range of numbers of bottles.
+  """
+  @spec lyrics(Range.t) :: String.t
+  def lyrics() do
+    lyrics(99..0)
+  end
+
+  def lyrics(a..b) do
+    a..b+1
+    |> Enum.reduce("", &(&2 <> verse(&1) <> "\n"))
+    |> Kernel.<>(verse(b))
+  end
+end
