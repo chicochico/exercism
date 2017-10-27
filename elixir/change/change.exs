ba11213f4af32fc3b9ce6c55af7b773885b667d6
chico <fxchiang@gmail.com>
2017-10-27 16:12:36 -0200

    change solution in elixir

--- elixir/change/change.exs
+++ elixir/change/change.exs
@@ -18,55 +18,94 @@ defmodule Change do
   """
 
   @spec generate(list, integer) :: {:ok, list} | {:error, String.t}
-  def generate(_, 0), do: {:ok, []}
-  def generate(_, target) when target < 0, do: {:error, "cannot change"}
-  def generate([smallest_coin|_], target) when smallest_coin > target, do: {:error, "cannot change"}
   def generate(coins, target) do
-    gen_accumulator(target)
-    |> make_change(coins)
+    cond do
+      target == 0 -> {:ok, []}
+      target < 0 -> {:error, "cannot change"}
+      hd(coins) > target -> {:error, "cannot change"}
+      true ->
+        # the cache store intermediate results
+        generate_cache(target)
+        |> make_change(coins)
+    end
   end
 
-  def make_change(memo, coins) do
-    1..Map.size(memo)-1
-    |> Enum.reduce(memo, fn i, acc ->
+  @doc """
+  Reduce on the cache storing intermediate results, the cache is a map,
+  with the key representing the index, and the value the intermediate results
+  """
+  @spec make_change(map, list) :: {:ok, list} | {:error, String.t}
+  def make_change(cache, coins) do
+    1..Map.size(cache)-1
+    |> Enum.reduce(cache, fn i, acc ->  # go thru each index in the cache
       coins
-      |> Enum.filter(&(&1 <= i))
+      |> Enum.filter(&(&1 <= i))  # remove coins that have value bigger than the index
       |> make_change(acc, i)
     end)
     |> fetch_coins()
   end
 
-  def make_change(coins, memo, index) do
+  @doc """
+  Match a empty coins list, and do nothing
+  """
+  @spec make_change(list, map, any) :: map
+  def make_change([], cache, _), do: cache
+
+  @doc """
+  Update the cache at index, with current iteration results
+  """
+  @spec make_change(list, map, integer) :: map
+  def make_change(coins, cache, index) do
     possible_vals =
       (for coin <- coins, do: index - coin)
-      |> Enum.reduce([], &(&2 ++ [memo[&1]]))
-      |> Enum.reduce([], fn [h|_], acc -> acc ++ [h+1] end)
+      |> Enum.reduce([], &(&2 ++ [cache[&1]]))
+      |> Enum.reduce([], fn [h|_], acc ->
+        case h do
+          nil -> acc ++ [h]
+          _ -> acc ++ [h+1]
+        end
+      end)
 
     count = Enum.min(possible_vals)
     used_coin = Enum.at(coins, Enum.find_index(possible_vals, &(&1 == count)))
 
-    memo
+    cache
     |> Map.put(index, [count, used_coin])
   end
 
-  def fetch_coins(memo) do
-    memo
+  @doc """
+  Fetch the coins that sum up to the change from cached results
+  """
+  @spec fetch_coins(map) :: list
+  def fetch_coins(cache) do
+    cache
     |> Enum.sort()
     |> Enum.map(fn {_, [_, coin]} -> coin end)
     |> Enum.reverse()
     |> fetch_coins(0, [])
   end
 
+  @spec fetch_coins(list, integer, list) :: {:ok, list} | {:error, String.t}
   def fetch_coins(coins, i, acc) do
     coin = Enum.at(coins, i)
-    if coin != 0 do
-      fetch_coins(coins, i+coin, acc ++ [coin])
-    else
-      {:ok, acc}
+    cond do
+      coin == nil -> {:error, "cannot change"}
+      coin == 0 -> {:ok, acc}
+      true -> fetch_coins(coins, i+coin, acc ++ [coin])
     end
   end
 
-  def gen_accumulator(target) do
+  @doc """
+  Generate a map that is used to store intermediate results,
+  %{0 => [0, 0], 1 => [nil, nil], 2 => [nil, nil]...}
+  the key is the index, and also a partial sum i.e. what coins sum up to that change,
+  the first element in the value is how many coins where used,
+  the second is the coin value that was used,
+  the first element of the cache is initialized with [0, 0]
+  it is a indicator to stop iteration when collecting the coins (see fetch_coins/3)
+  """
+  @spec generate_cache(integer) :: map
+  def generate_cache(target) do
     (for i <- 0..target, do: {i, [nil, nil]})
     |> Enum.into(%{})
     |> Map.put(0, [0, 0])
