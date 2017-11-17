501a3810ac1677f8a7d97eeea02daf02418eba38
chico <fxchiang@gmail.com>
2017-11-17 01:27:18 -0200

    sieve in elixir

---
+++ elixir/sieve/sieve.exs
@@ -0,0 +1,19 @@
+defmodule Sieve do
+
+  @doc """
+  Generates a list of primes up to a given limit.
+  """
+  @spec primes_to(non_neg_integer) :: [non_neg_integer]
+  def primes_to(limit) do
+    2..limit
+    |> Enum.to_list
+    |> primes_to([])
+  end
+
+  @spec primes_to(list, list) :: list
+  def primes_to([], acc), do: acc
+  def primes_to([h|t], acc) do
+    (for n <- t, rem(n, h) != 0, do: n)
+    |> primes_to(acc ++ [h])
+  end
+end
