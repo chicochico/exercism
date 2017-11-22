278e9c938d2590dac3f1bf4f86f8239fdf687d73
chico <fxchiang@gmail.com>
2017-11-22 10:10:56 -0200

    crypto square in elixir

---
+++ elixir/crypto-square/crypto_square.exs
@@ -0,0 +1,33 @@
+defmodule CryptoSquare do
+  @doc """
+  Encode string square methods
+  ## Examples
+
+    iex> CryptoSquare.encode("abcd")
+    "ac bd"
+  """
+  @spec encode(String.t) :: String.t
+  def encode(str) do
+    str =
+      str
+      |> String.downcase
+      |> String.replace(" ", "")
+      |> String.replace(~r/[[:punct:]]/, "")
+      |> String.graphemes
+
+    columns = find_column_count(length str)
+    padding = List.duplicate("_", columns)
+
+    str
+    |> Enum.chunk_every(columns, columns, padding)
+    |> Enum.zip
+    |> Enum.map(&Tuple.to_list(&1))
+    |> Enum.map(&Enum.join(&1))
+    |> Enum.join(" ")
+    |> String.replace("_", "")
+  end
+
+  def find_column_count(len), do: find_column_count(len, 1)
+  def find_column_count(len, n) when n*n >= len, do: n
+  def find_column_count(len, n), do: find_column_count(len, n+1)
+end
