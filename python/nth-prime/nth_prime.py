0ac75e3ad628b648ae82f15f283086401ccd4bf7
chico <fxchiang@gmail.com>
2018-01-29 16:20:25 -0200

    faster

--- python/nth-prime/nth_prime.py
+++ python/nth-prime/nth_prime.py
@@ -1,20 +1,27 @@
-def nth_prime(count):
-    primes = [2]
+import math
 
-    if count == 1:
-        return 2
-    elif count < 1:
+
+def nth_prime(count):
+    if count < 1:
         raise ValueError('Minimum of one.')
 
+    primes = [2]
     current = 3
     while len(primes) < count:
-        for p in primes:
-            if current % p == 0:
-                current += 1
-                break
-        else:
-            # if for terminates without break
+        if is_prime(current, primes):
             primes.append(current)
-            current += 1
+            current += 2
+        else:
+            current += 2
 
     return primes[-1]
+
+
+def is_prime(number, primes):
+    limit = math.floor(math.sqrt(number))
+    for p in primes:
+        if number % p == 0:
+            return False
+        if p > limit:
+            break
+    return True
