b0341aec80c37cb0c8b89432b31e95424c137caf
chico <fxchiang@gmail.com>
2018-01-31 13:29:30 -0200

    readability improvement

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,28 +1,20 @@
-import math
-
-
 def largest_palindrome(min_factor, max_factor):
     if min_factor > max_factor:
         raise ValueError('Invalid factors.')
 
-    start = max_factor
-
     # reduce the search space
     if max_factor < 10:
-        stop = min_factor - 1
-        factors = [(a, b) for a in range(start, stop, -1)
-                   for b in range(a, stop, -1)
+        factors = [(a, b) for a in range(max_factor, min_factor-1, -1)
+                   for b in range(a, min_factor-1, -1)
                    if is_palindrome(a * b)]
         palindrome = max(a * b for (a, b) in factors)
         factors = {(a, b) for (a, b) in factors if a * b == palindrome}
         return (palindrome, factors)
-    elif max_factor < 100:
-        stop = math.floor(max_factor * 0.8)
-    else:
-        stop = math.floor(max_factor * 0.9)
 
-    for a in range(start, stop, -1):
-        for b in range(a, stop, -1):
+    min_factor = int(max_factor * 0.8) if max_factor < 100 else int(max_factor * 0.9)
+
+    for a in range(max_factor, min_factor, -1):
+        for b in range(a, min_factor, -1):
             if is_palindrome(a * b):
                 return (a * b, {(a, b)})
 
