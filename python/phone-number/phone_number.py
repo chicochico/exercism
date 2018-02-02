95cbeb4c33c0cfb29d483fadff7bfabdbb6a9eb3
chico <fxchiang@gmail.com>
2018-02-02 17:23:28 -0200

    refactor parse function

--- python/phone-number/phone_number.py
+++ python/phone-number/phone_number.py
@@ -1,6 +1,3 @@
-import re
-
-
 class Phone(object):
     def __init__(self, phone_number):
         (self.area_code,
@@ -15,18 +12,16 @@ class Phone(object):
         return f'({self.area_code}) {self.exchange_code}-{self.unique}'
 
     def parse(self, number):
-        number = re.findall(r'\d', number)
-        country_code = '1'
-        if not 9 < len(number) < 12:
+        number = ''.join(filter(str.isdigit, number))
+
+        if (not 9 < len(number) < 12 or
+                len(number) == 11 and number[0] != '1' or
+                int(number[-10]) < 2 or
+                int(number[-7]) < 2):
+
             raise ValueError('Invalid number.')
-        if len(number) == 11:
-            country_code = number[0]
-            number = number[1:]
-        area_code = ''.join(number[:3])
-        exchange_code = ''.join(number[3:6])
-        unique = ''.join(number[6:])
-        if (int(area_code[0]) < 2 or
-                int(exchange_code[0]) < 2 or
-                country_code != '1'):
-            raise ValueError('Invalid number.')
+
+        area_code = number[-10:-7]
+        exchange_code = number[-7:-4]
+        unique = number[-4:]
         return area_code, exchange_code, unique
