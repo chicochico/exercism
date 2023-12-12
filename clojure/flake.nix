cfe76ef299298baf85e2d04177f0b9b170bb1e20
Francisco Chiang <mail@fchiang.com>
2023-12-12 22:20:54 +0100

    Add exercism CLI

--- clojure/flake.nix
+++ clojure/flake.nix
@@ -13,7 +13,7 @@
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ clojure leiningen ];
+          packages = with pkgs; [ exercism clojure leiningen ];
         };
       });
 }
