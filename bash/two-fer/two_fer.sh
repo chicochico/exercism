14bc061d5d90ff6a9a45df98f4867cf9da872068
Francisco Chiang <mail@fchiang.com>
2024-05-01 07:10:15 +0200

    Start bash track

---
+++ bash/two-fer/two_fer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+main() {
+    local name
+    name="$1"
+    if [ -z "$name" ]; then
+        echo "One for you, one for me."
+    else
+        echo "One for ${name}, one for me."
+    fi
+}
+
+main "$@"
