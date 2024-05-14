23fd95f0b9dc71bcb58300a58682dcaf05d33fce
Francisco Chiang <mail@fchiang.com>
2024-05-14 06:52:11 +0200

    Add acronym

---
+++ bash/acronym/acronym.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Using parameter expansions
+
+# ${variable//search/replace}
+# search: will match three characters - or _ or *
+# first clean up dashes and glob character
+# then word split
+phrase="${*//[-_*]/ }"
+for word in $phrase; do
+    result+="${word:0:1}"
+done
+
+# ${parameter@operator} - U converts parameter to uppercase
+echo "${result@U}"
